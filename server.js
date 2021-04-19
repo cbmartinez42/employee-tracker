d628eaf3ffa0db812374437561ed7142cf046ce7
Chris Martinez <cbmartinez42@gmail.com>
2021-04-18 22:20:29 -0500

    update with working sql seed and some functions with inquirer

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const table = require('console.table');
-require('custom-env').env(false);
+require('custom-env').env(true);
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -13,12 +13,12 @@ const connection = mysql.createConnection({
 
 const start = () => {
     console.log('');
-    console.log('EEEEEEE                     lll                                 DDDDD           tt            bb                          ');
+    console.log('EEEEEEE                     lll                                 DDDDD           tt            bb                           ');
     console.log('EE      mm mm mmmm  pp pp   lll  oooo  yy   yy   eee    eee     DD  DD    aa aa tt      aa aa bb        aa aa  sss    eee  ');
-    console.log('EEEEE   mmm  mm  mm ppp  pp lll oo  oo yy   yy ee   e ee   e    DD   DD  aa aaa tttt   aa aaa bbbbbb   aa aaa s     ee   e');
+    console.log('EEEEE   mmm  mm  mm ppp  pp lll oo  oo yy   yy ee   e ee   e    DD   DD  aa aaa tttt   aa aaa bbbbbb   aa aaa s     ee   e ');
     console.log('EE      mmm  mm  mm pppppp  lll oo  oo  yyyyyy eeeee  eeeee     DD   DD aa  aaa tt    aa  aaa bb   bb aa  aaa  sss  eeeee  ');
-    console.log('EEEEEEE mmm  mm  mm pp      lll  oooo       yy  eeeee  eeeee    DDDDDD   aaa aa  tttt  aaa aa bbbbbb   aaa aa     s  eeeee');
-    console.log('                    pp                  yyyyy                                                                  sss        ');
+    console.log('EEEEEEE mmm  mm  mm pp      lll  oooo       yy  eeeee  eeeee    DDDDDD   aaa aa  tttt  aaa aa bbbbbb   aaa aa     s  eeeee ');
+    console.log('                    pp                  yyyyy                                                                  sss         ');
     console.log('');
 
     inquirer
@@ -26,16 +26,20 @@ const start = () => {
         name: 'functionChoice',
         type: 'list',
         message: 'Hello. What would you like to do, O Great One?',
-        choices: ['Add departments, roles, or employees', 'View departments, roles, or employees', 'Update employee roles', 'EXIT'],
+        choices: ['Add an employee', 'View departments', 'View roles', 'View all employees', 'Update employee roles', 'EXIT'],
       })
       .then((answer) => {
         // based on their answer, either call the bid or the post functions
-        if (answer.functionChoice === 'Add departments, roles, or employees') {
-          addToDB();
-        } else if (answer.functionChoice === 'View departments, roles, or employees') {
-          viewDB();
+        if (answer.functionChoice === 'Add an employee') {
+            addToDB();
+        } else if (answer.functionChoice === 'View departments') {
+            viewDept();
+        } else if (answer.functionChoice === 'View roles') {
+            viewRoles();
+        } else if (answer.functionChoice === 'View all employees') {
+            viewEmployees();
         } else if (answer.functionChoice === 'Update employee roles') {
-          updateDB();
+            updateRole();
         } else {
           connection.end();
         }
@@ -47,12 +51,68 @@ const addToDB = () => {
 
 };
 
-const viewDB = () => {
+const viewDept = () => {
+    connection.query('SELECT name FROM department'), (err, res) => {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: 'department',
+                    type: 'list',
+                    choices() {
+                        const departmentArray = [];
+                        res.forEach(({ name }) => {
+                            departmentArray.push(name);
+                        });
+                        return departmentArray
+                    },
+                    message: 'Which department would you like to view?',
+                }
+            ])
+            .then((answer) => {
+                connection.query(`SELECT * FROM `) // <------------------ NEED TO FINISH THIS -------------------------------
+            })
+    }
+    
+};
+
+const viewRoles = () => {
+    connection.query('SELECT title FROM role'), (err, res) => {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: 'role',
+                    type: 'list',
+                    choices() {
+                        const roleArray = [];
+                        res.forEach(({ title }) => {
+                            roleArray.push(title);
+                        });
+                        return roleArray
+                    },
+                    message: 'Which role would you like to view?'
+                }
+            ])
+            .then((answer) => {
+                connection.query(`SELECT * FROM `) // <----------------------need to finish this --------------------
+            })
+    }
+    
+};
+
+const viewEmployees = () => {
+    connection.query('SELECT * FROM employee_db.employee'), (err, res) => {
+        if (err) throw err;
+        // const values = res;
+        // console.table('All Employees', ['First Name', 'Last Name', 'Role ID', 'Manager ID'], values)
+        console.table(res);
+    }
 
     
 };
 
-const updateDB = () => {
+const updateRole = () => {
 
     
 };
--- sql/seed.sql
+++ sql/seed.sql
@@ -3,24 +3,40 @@ CREATE DATABASE employee_DB;
 
 USE employee_DB;
 
-CREATE TABLE employee(
+CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT default 0,
-  manager_id INT default 0,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  PRIMARY KEY (id)
+  salary DECIMAL(30) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
-CREATE TABLE department(
+CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role (id),
+  FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Human Resources"), ("Finance"), ("Engineering"), ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), ("Salesperson", 60000, 1), ("HR Manager", 80000, 2), ("Recruiter", 60000, 2), 
+("Accounting Lead", 80000, 3), ("Accountant", 60000, 3), ("Lead Engineer", 120000, 4), ("Engineer", 90000, 4),
+("Legal Lead", 150000, 5), ("Lawyer", 120000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ash", "Williams", 1, null), ("Grogu", "Babyyoda", 2, 1), ("Jean-Luc", "Picard", 3, null), ("William", "Riker", 4, 3),
+("Luke", "Skywalker", 5, null), ("Han", "Solo", 6, 5);
