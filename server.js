aa125fd2fa5d358a089cb16b6436a9714ec90dce
Chris Martinez <cbmartinez42@gmail.com>
2021-04-20 12:32:49 -0500

    committing with working joins for all employee table

--- server.js
+++ server.js
@@ -2,24 +2,36 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const table = require('console.table');
 require('custom-env').env(true);
+// const chalk = require('chalk');
+// const error = chalk.bold.red;
+const colors = require('colors');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    port: process.env.PORT || 3306,
     user: process.env.USER || '',
     password: process.env.PASSWORD || '',
-    database: 'employee_DB'
+    database: 'employee_DB',
 });
 
+const title = () => {
+
+    console.log('');
+    console.log('∙∙∙∙∙·ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ∙∙∙∙∙·'.bgBlue);
+    console.log('.                                                                                                                            .'.bgBlue);
+    console.log('. EEEEEEE                     lll                                 DDDDD           tt            bb                           .'.bgBlue);
+    console.log('. EE      mm mm mmmm  pp pp   lll  oooo  yy   yy   eee    eee     DD  DD    aa aa tt      aa aa bb        aa aa  sss    eee  .'.bgBlue);
+    console.log('. EEEEE   mmm  mm  mm ppp  pp lll oo  oo yy   yy ee   e ee   e    DD   DD  aa aaa tttt   aa aaa bbbbbb   aa aaa s     ee   e .'.bgBlue);
+    console.log('. EE      mmm  mm  mm pppppp  lll oo  oo  yyyyyy eeeee  eeeee     DD   DD aa  aaa tt    aa  aaa bb   bb aa  aaa  sss  eeeee  .'.bgBlue);
+    console.log('. EEEEEEE mmm  mm  mm pp      lll  oooo       yy  eeeee  eeeee    DDDDDD   aaa aa  tttt  aaa aa bbbbbb   aaa aa     s  eeeee .'.bgBlue);
+    console.log('.                     pp                  yyyyy                                                                  sss         .'.bgBlue);
+    console.log('.                                                                                                                            .'.bgBlue);
+    console.log('∙∙∙∙∙·ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ∙∙∙∙∙·'.bgBlue);
+    console.log('');
+    start();
+}
+
 const start = () => {
-    console.log('');
-    console.log('EEEEEEE                     lll                                 DDDDD           tt            bb                           ');
-    console.log('EE      mm mm mmmm  pp pp   lll  oooo  yy   yy   eee    eee     DD  DD    aa aa tt      aa aa bb        aa aa  sss    eee  ');
-    console.log('EEEEE   mmm  mm  mm ppp  pp lll oo  oo yy   yy ee   e ee   e    DD   DD  aa aaa tttt   aa aaa bbbbbb   aa aaa s     ee   e ');
-    console.log('EE      mmm  mm  mm pppppp  lll oo  oo  yyyyyy eeeee  eeeee     DD   DD aa  aaa tt    aa  aaa bb   bb aa  aaa  sss  eeeee  ');
-    console.log('EEEEEEE mmm  mm  mm pp      lll  oooo       yy  eeeee  eeeee    DDDDDD   aaa aa  tttt  aaa aa bbbbbb   aaa aa     s  eeeee ');
-    console.log('                    pp                  yyyyy                                                                  sss         ');
-    console.log('');
 
     inquirer
       .prompt({
@@ -29,7 +41,8 @@ const start = () => {
         choices: ['Add an employee', 'View departments', 'View roles', 'View all employees', 'Update employee roles', 'EXIT'],
       })
       .then((answer) => {
-        // based on their answer, either call the bid or the post functions
+        // based on their answer, call the appropriate function
+        // switch/case? 
         if (answer.functionChoice === 'Add an employee') {
             addToDB();
         } else if (answer.functionChoice === 'View departments') {
@@ -101,16 +114,28 @@ const viewRoles = () => {
     
 };
 
-const viewEmployees = () => {
-    connection.query('SELECT * FROM employee_db.employee'), (err, res) => {
-        if (err) throw err;
-        // const values = res;
-        // console.table('All Employees', ['First Name', 'Last Name', 'Role ID', 'Manager ID'], values)
-        console.table(res);
-    }
-
+// const viewEmployees = () => 
+//     new Promise ((resolve, reject) => {
+//         connection.query('SELECT * FROM employee_db.employee', (err, res) => {
+//         if (err) reject(new Error('Oops! Something went wrong --->', err));
+//         const employees = console.table(res);
+//         resolve (employees);
+//         start();
+//     });
     
-};
+// });
+
+const viewEmployees =  () => 
+    new Promise ((resolve, reject) => {
+        connection.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary,department.name AS department, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department_id = department.id LEFT JOIN employee manager on employee.manager_id = manager.id', (err, res) => {
+        // if (err) reject(new Error(" Oops! Something went wrong ¯\_(ツ)_/¯ ".bold.bgRed, err));
+        const employees = console.table(res);
+        resolve (employees);
+        start();
+    });
+        
+    
+}).catch(err => console.error(" Oops! Something went wrong ¯\_(ツ)_/¯ ".bold.bgRed, err));
 
 const updateRole = () => {
 
@@ -119,13 +144,9 @@ const updateRole = () => {
 
 
 
-
-
-
-
 // connect to the mysql server and sql database
 connection.connect((err) => {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
-    start();
+    title();
   });
